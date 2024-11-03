f4b6586a771a5dee6747ce5642c13ea09b6c00d2
Nikita Raikov <raykiri1998@mail.ru>
2024-11-03 11:16:30 +0400

    dss

--- sniper.py
+++ sniper.py
@@ -127,7 +127,7 @@ async def main() -> None:
     # Запуск проверки по базе данныех в 10:00 мск
     application.job_queue.run_daily(
     schedule_daily_check,
-    time=datetime.time(hour=10, minute=10, tzinfo=pytz.timezone('Europe/Moscow'))
+    time=datetime.time(hour=10, minute=20, tzinfo=pytz.timezone('Europe/Moscow'))
 )
 
 
