03c88adcf11c8cacec0166367017d1f913ee8781
Nikita Raikov <raykiri1998@mail.ru>
2024-11-04 16:46:00 +0400

    wpppg

--- formirovanie_zaprosa.py
+++ formirovanie_zaprosa.py
@@ -55,7 +55,7 @@ async def get_diapazon_revenue(update: Update, context: CallbackContext) -> int:
         await db.commit()
 
     # Теперь формируем и отправляем запрос к MPStats с использованием введенных данных
-    await make_mpstats_request(update, context, user_id)
+    await update.message.reply_text("Ваши фильтры сохранены. Чтобы получить товары, введите команду /start_request")
     return ConversationHandler.END
 
 # Выход из диалога
